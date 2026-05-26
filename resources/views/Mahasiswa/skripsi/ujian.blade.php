db7473350854f477cbfd9c1a400138cc4feb5c37
unknown <muhdemas2003@gmail.com>
2026-05-26 14:05:06 +0700

    update button konfirmasi

--- resources/views/Mahasiswa/skripsi/ujian.blade.php
+++ resources/views/Mahasiswa/skripsi/ujian.blade.php
@@ -233,6 +233,16 @@
     var isBayarUjianLunas = false;
     var taIsObe = true; // Default supports OBE
 
+    function setSubmitUjianButtonState(isLocked) {
+        var btn = $('#btn-submit-final');
+
+        if (isLocked) {
+            btn.prop('disabled', true).html('<i class="fa fa-check mr-10"></i> Pendaftaran Sudah Terkirim');
+        } else {
+            btn.prop('disabled', false).html('<i class="fa fa-paper-plane mr-10"></i> Konfirmasi Pendaftaran Ujian');
+        }
+    }
+
     $(document).ready(function() {
         // Luaran Type Change listener
         $('#select_jenis_luaran').on('change', function() {
@@ -548,6 +558,9 @@
                         `).show();
                     }
 
+                    ujianLocked = !!res.data.ujian_locked;
+                    setSubmitUjianButtonState(ujianLocked);
+
                     const target = res.data.target_luaran || 'buku_skripsi';
                     const luaran = res.data.luaran;
                     
@@ -643,7 +656,7 @@
                                 errorMsg = err.responseJSON.error;
                             }
                             showToastr('error', 'Gagal', errorMsg);
-                            btn.prop('disabled', false).html('Konfirmasi Pendaftaran Ujian');
+                            setSubmitUjianButtonState(false);
                         }
                     });
                 },
@@ -653,7 +666,7 @@
                         errorMsg += ' ' + (Array.isArray(err.responseJSON.error) ? err.responseJSON.error.join(', ') : err.responseJSON.error);
                     }
                     showToastr('error', 'Gagal', errorMsg);
-                    btn.prop('disabled', false).html('Konfirmasi Pendaftaran Ujian');
+                    setSubmitUjianButtonState(false);
                 }
             });
         });
